802f01961ad953597eed0c8252fd3269ae9fae85
natalia.ewa.bielecka@gmail.com <93135239+neb-dev-il@users.noreply.github.com>
2022-05-17 19:54:21 +0200

    Adds a StringChallenge class and method

---
+++ Algorithms/Algorithms/src/StringChallenge1.java
@@ -0,0 +1,30 @@
+class StringChallenge1 {
+
+    static String StringChallenge(String str) {
+        // code goes here
+        String output = str;
+        str = "";
+
+        for (int j = 0; j < output.length();) {
+
+            int counter = 0;
+
+            for (int i = j; i < output.length(); i++) {
+
+                if (output.charAt(j) != output.charAt(i)) {
+                    break;
+                }
+
+                ++counter;
+
+            }
+
+            str = str.concat(String.valueOf(counter)).concat(String.valueOf(output.charAt(j)));
+            j += counter;
+
+        }
+
+        return str;
+
+    }
+}
