7e8c741224d68f5235ac69e46734b5e0f6eceef9
natalia.ewa.bielecka@gmail.com <93135239+neb-dev-il@users.noreply.github.com>
2022-05-17 20:28:01 +0200

    Update Main.java

--- Algorithms/Algorithms/src/Main.java
+++ Algorithms/Algorithms/src/Main.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print(StringChallenge1.StringChallenge(s.nextLine()));
+        System.out.println(StringChallenge1.StringChallenge(s.nextLine()));
+        System.out.println(StringChallenge2.StringChallenge(s.nextLine()));
+        System.out.println(ArrayChallengeClass.ArrayChallenge(convertToArray(s.nextLine())));
+    }
+
+    private static int[] convertToArray(String nextLine) {
+        nextLine = nextLine.replace("new int[] {", "").replace(",", " ").replace("}", "");
+        String[] stringArray = nextLine.split(" ");
+        int[] intArray = new int[stringArray.length];
+
+        for (int i = 0; i < stringArray.length; i++) {
+            intArray[i] = Integer.parseInt(stringArray[i]);
+        }
+
+        return intArray;
+
     }
 }
