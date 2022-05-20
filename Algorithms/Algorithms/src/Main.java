56d29dd816048a27686166baaf7ec76777c1933e
natalia.ewa.bielecka@gmail.com <93135239+neb-dev-il@users.noreply.github.com>
2022-05-20 16:56:00 +0200

    Update Main.java

--- Algorithms/Algorithms/src/Main.java
+++ Algorithms/Algorithms/src/Main.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
+        /*
         System.out.println(StringChallenge1.StringChallenge(s.nextLine()));
         System.out.println(StringChallenge2.StringChallenge(s.nextLine()));
         System.out.println(ArrayChallengeClass.convertToArray(s.nextLine()));
         AlterCaseClass.AlterCase(s.nextLine());
+        NumberPairsClass.NumberPairs(s.nextLine());
+         */
     }
 }
