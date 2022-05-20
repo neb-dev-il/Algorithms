95820600e533dec99fd45a7c02b3ce2e4386fce5
natalia.ewa.bielecka@gmail.com <93135239+neb-dev-il@users.noreply.github.com>
2022-05-20 16:47:42 +0200

    Update Main.java

--- Algorithms/Algorithms/src/Main.java
+++ Algorithms/Algorithms/src/Main.java
@@ -1,10 +1,12 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
         System.out.println(StringChallenge1.StringChallenge(s.nextLine()));
         System.out.println(StringChallenge2.StringChallenge(s.nextLine()));
         System.out.println(ArrayChallengeClass.convertToArray(s.nextLine()));
+        AlterCaseClass.AlterCase(s.nextLine());
     }
 }
